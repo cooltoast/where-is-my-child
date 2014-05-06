cc206d6da46367ce0167476c335a41f6e1396b9d
cooltoast <mpatel678@gmail.com>
2014-05-05 20:27:37 -0400

    add button and retrieve latitute/longitude

--- app/src/main/java/com/cooltoast/whereismychild/app/MainActivity.java
+++ app/src/main/java/com/cooltoast/whereismychild/app/MainActivity.java
@@ -1,17 +1,59 @@
 package com.cooltoast.whereismychild.app;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.button1);
+        final LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 50, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+               Toast.makeText(MainActivity.this, "Latitude: " + location.getLatitude() + " Longitude: " + location.getLongitude(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+                Toast.makeText(MainActivity.this, "GPS enabled", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Toast.makeText(MainActivity.this, "GPS disabled", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
